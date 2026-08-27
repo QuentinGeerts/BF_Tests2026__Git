a1ffd51c8e94e49881509b69244d828d1e88bb77
Quentin Geerts <quentin.geerts@bstorm.be>
2026-08-27 15:11:16 +0200

    Update /testeurs route to return the correct current time message

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/testeurs", (req, res) => {
-  return res.status(200).json({ message: "Il est 14h25" });
+  return res.status(200).json({ message: "Il est 15h10" });
 })
 
 app.listen(PORT, () => 
