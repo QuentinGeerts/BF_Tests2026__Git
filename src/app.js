c67ce1520d5ac0e0fbb23f99dded29695825dc00
Quentin Geerts <quentin.geerts@bstorm.be>
2026-08-27 15:59:29 +0200

    Fix #2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/testeurs", (req, res) => {
-  return res.status(200).json({ message: "Il est 15h55." });
+  return res.status(200).json({ message: "Il est 15h59." });
 });
 
 app.listen(PORT, () =>
